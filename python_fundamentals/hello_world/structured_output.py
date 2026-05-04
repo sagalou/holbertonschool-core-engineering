8eea78e4416531310c8e9b64901eead607488d4d
sagalou <sagal.louise.haider@gmail.com>
2026-05-04 14:31:15 +0200

    fix line 7 in the structured_output.py file

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 print('Language: Python')
-print ('Version: 3')
+print('Version: 3')
 Pi_approx = 3.14
 print(f"Pi approx: {Pi_approx:.2f}")
 Computation_valid = Pi_approx < 10
