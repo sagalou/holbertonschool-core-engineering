e309ebb99b13fda33edb03d61eaa6e6978bb8ebd
sagalou <sagal.louise.haider@gmail.com>
2026-05-04 14:23:57 +0200

    Add file structured_output.py

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+print('Language: Python')
+print ('Version: 3')
+Pi_approx = 3.14
+print(f"Pi approx: {Pi_approx:.2f}")
+Computation_valid = Pi_approx < 10
+print(f"Computation valid: {Computation_valid}")
