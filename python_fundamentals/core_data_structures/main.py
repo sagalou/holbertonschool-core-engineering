f6da068ae0786724c707838116f273d783a2c0a5
sagalou <sagal.louise.haider@gmail.com>
2026-05-07 16:16:06 +0200

    add main.py

---
+++ python_fundamentals/core_data_structures/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+replace_in_list = __import__('replace_in_list').replace_in_list
+
+my_list = [1, 2, 3, 4, 5]
+print(replace_in_list(my_list, 3, 99))
+print(replace_in_list(my_list, 15, 99))
