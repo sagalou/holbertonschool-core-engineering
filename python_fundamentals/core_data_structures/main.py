e86bd5cae89db100b16436918929395bcfa6c991
sagalou <sagal.louise.haider@gmail.com>
2026-05-07 16:18:43 +0200

    add main.py

--- python_fundamentals/core_data_structures/main.py
+++ python_fundamentals/core_data_structures/main.py
@@ -4,3 +4,6 @@ replace_in_list = __import__('replace_in_list').replace_in_list
 my_list = [1, 2, 3, 4, 5]
 print(replace_in_list(my_list, 3, 99))
 print(replace_in_list(my_list, 15, 99))
+
+my_list = [1, 2, 3]
+print(replace_in_list(my_list, 1, 4))
