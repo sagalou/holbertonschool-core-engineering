79d54149dfd5680a91b172140f057914b4a90ec4
sagalou <sagal.louise.haider@gmail.com>
2026-05-12 01:02:29 +0200

    fix 1-square.py file with doc

--- python_oop/classes_and_object_model/1-square.py
+++ python_oop/classes_and_object_model/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""Module that defines a Square class"""
+
+
 class Square:
+    """Class that represents a square"""
+
     def __init__(self, size):
+        """Initialize a new Square instance with a given size"""
         self.__size = size
