f705d80002c6ccb88872a444329f0087c6a2989f
sagalou <sagal.louise.haider@gmail.com>
2026-05-12 12:55:31 +0200

    add 6-square.py file

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -34,7 +34,7 @@ class Square:
         """Setter for position"""
         if (not isinstance(value, tuple) or len(value) != 2
                 or not all(isinstance(i, int) and i >= 0 for i in value)):
-            raise TypeError("position must be a tuple of 2 positive integer")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
