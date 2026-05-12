85a26689f0ce9d3119deda60d5181da1e05f3c5f
sagalou <sagal.louise.haider@gmail.com>
2026-05-12 12:29:40 +0200

    add 4-square.py file

---
+++ python_oop/classes_and_object_model/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Module that adds getter and setter for size attribute"""
+
+
+class Square:
+
+    """Class that represents a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square instance"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size * self.__size
