edc7b5960336730db6652e7ea595e868576a1d94
sagalou <sagal.louise.haider@gmail.com>
2026-05-13 20:35:30 +0200

    fix base_geometry.py file

--- python_oop/inheritance/base_geometry.py
+++ python_oop/inheritance/base_geometry.py
@@ -9,11 +9,8 @@ class BaseGeometry:
         raise Exception
 
     def integer_validator(self, name, value):
-        """Validates values:
-        - must be an integer (TypeError)
-        - must be greater than 0 'ValueError)
-        """
+        """Validates values as a positive integer"""
         if type(value) is not int:
-            raise TypeError("{}must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{}must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
