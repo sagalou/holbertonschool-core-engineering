48453ac7743e312908ab1ac2c4b0eadb657c9eef
sagalou <sagal.louise.haider@gmail.com>
2026-05-15 11:52:05 +0200

    add 2-square.py file

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module for Square class"""
+
+Rectangle = __import__('2-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initializes Square with size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Computes the area of the square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """String representation"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
